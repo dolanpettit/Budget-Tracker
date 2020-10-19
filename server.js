7baffa4327b839827a57e94e92a5edb5a5baec4c
Dolan Pettit <dolanpettit@gmail.com>
2020-10-19 11:55:55 -0400

    Update: Adding boilerplate code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
@@ -9,11 +9,11 @@ const app = express();
 
 app.use(logger("dev"));
 
-app
-  .use(compression())
-  .use(express.urlencoded({ extended: true }))
-  .use(express.json())
-  .use(express.static("public"));
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
