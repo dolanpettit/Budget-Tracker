6c2cf3a582191379eb625e4858b212cddcf5a0fb
Dolan Pettit <dolanpettit@gmail.com>
2020-10-19 13:33:46 -0400

    Update: Created logic for serviceWorker to install, and activate

--- public/sWorker.js
+++ public/sWorker.js
@@ -10,3 +10,31 @@ const FILES_TO_CACHE = [
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
+
+// Service Worker Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME),
+    caches.then((cache) => cache.addAll(FILES_TO_CACHE)),
+    caches.then(self.skipWaiting())
+  );
+});
+
+// Service Worker Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Service Worker Fetch
+
