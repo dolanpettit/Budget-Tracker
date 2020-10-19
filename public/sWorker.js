5a39cab36e9940d949a33c58c877ed6733f045f5
Dolan Pettit <dolanpettit@gmail.com>
2020-10-19 13:04:46 -0400

    Update: Created serviceWorker and added files to cache

---
+++ public/sWorker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "static-cache-v3";
+const DATA_CACHE_NAME = "data-cache-v4";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+];
